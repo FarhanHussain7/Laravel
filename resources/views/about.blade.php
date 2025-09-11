736a225ab9fcaf310dff8751229815a8a78989c7
Farhanhussain7 <farhanhussain81303@gmail.com>
2025-09-11 23:06:11 +0530

    Routes working

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<h1>About Page</h1>
+<a href="/">Welcome page</a>
+<a href="/about/farhan">Farhan page</a>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hoes', function () {
     return view('Home');
+});
+
+Route::get('/about/{name}', function ($name) {
+    return view('about',['name'=>$name]);
 });
